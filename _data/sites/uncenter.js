ed9dc0eed360a9f35c8f913fcfcd7eb4aa56bcbf
uncenter <47499684+uncenter@users.noreply.github.com>
2023-01-13 22:52:02 -0700

    Update and rename sample.js to uncenter.js

---
+++ _data/sites/uncenter.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "uncenter.org", // optional, falls back to object key
+	description: "My blog and portfolio site.",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.uncenter.org/"
+	]
+};
