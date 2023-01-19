e4d79bada8122273a4b18959b3e11dbdacf5872c
uncenter <47499684+uncenter@users.noreply.github.com>
2023-01-19 09:11:22 -0500

    Update and rename uncenter.js to Sites.js

---
+++ _data/sites/Sites.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Sites", // optional, falls back to object key
+	description: "Sites of people I know",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.uncenter.org/",
+		"https://shivjm.blog/",
+		"https://boehs.org/",
+		"https://dpnkr.in/",
+		"https://www.puruvj.dev/"
+	]
+};
