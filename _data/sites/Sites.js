88017a6ab5b5c43b274d96020d0fdfebbf98c16a
uncenter <me@uncenter.org>
2023-01-23 00:43:54 -0500

    minor changes

--- _data/sites/Sites.js
+++ _data/sites/Sites.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "Sites", // optional, falls back to object key
-	description: "Sites of people I know",
+	description: "just some websites",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
